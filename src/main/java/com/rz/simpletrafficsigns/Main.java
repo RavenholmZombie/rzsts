0c263368941f463a8175559436acbb8a2a24da06
Chris <supersoftware1234@gmail.com>
2024-03-25 08:13:25 -0400

    Begin

---
+++ src/main/java/com/rz/simpletrafficsigns/Main.java
@@ -0,0 +1,26 @@
+package com.rz.simpletrafficsigns;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Main.MOD_ID)
+public class Main {
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "rzsts";
+
+    public Main()
+    {
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+}
